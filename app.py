b46f6f16d1e1626034e5e1d79ba8e913d649a680
EvaYaroslav12 <eva.yaroslavskiy@gmail.com>
2026-01-28 09:24:04 -0500

    turtle.test

--- app.py
+++ app.py
@@ -1,1 +1,22 @@
-print("tests")
+import turtle
+from turtle import *
+t = Turtle()
+
+t.color("blue")
+def triangle(x):
+    t.forward(90)
+    t.left(90)
+    t.forward(90)
+    t.left(90)
+    t.forward(90)
+    t.left(90)
+    t.forward(90)
+    t.left(90)
+triangle(200)
+
+t.penup
+left (90)
+t.pendown
+t.color("red")
+t.circle(90)
+turtle.done()
---
+++ test.py
@@ -0,0 +1,9 @@
+import turtle
+from turtle import *
+t = Turtle()
+
+
+t.forward(100)
+t.left(100)
+
+turtle.done()
