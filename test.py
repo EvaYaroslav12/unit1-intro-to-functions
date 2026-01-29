7216345407a32c3b2b4ff9ccc308a6ac8ef11435
EvaYaroslav12 <eva.yaroslavskiy@gmail.com>
2026-01-29 09:24:36 -0500

    Create test.py

---
+++ test.py
@@ -0,0 +1,25 @@
+import turtle
+from turtle import *
+t = Turtle()
+
+t.speed (20)
+
+sidelength = 100
+rotate = 90
+def square(x,y):
+    for i in range(4):
+        t.forward(x)
+        t.left(y)
+
+def addSquares(iRange):
+    length = 100
+
+    for i in range(iRange):
+        square(length, 90) 
+        t.left(5)
+       
+            
+
+addSquares(72)
+
+turtle.done()
